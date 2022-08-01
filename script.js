c73f3900125bafa4ea3a3fb77aa5488514abe9cb
Johan Delao <johandelao10@gmail.com>
2022-07-31 20:53:42 -0400

    connected with API, still needs work

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const modal = document.getElementById("appear");
 const botLine = document.getElementById("botLine");
 const topLine = document.getElementById("topLine");
 
+const URL = `https://pokeapi.co/api/v2/pokemon/`; // API Link
+let newURL = "";
+
 function myFunction(){
     if(modal.classList.contains("dontShow")){
         modal.classList.remove("dontShow");
@@ -17,4 +20,42 @@ function myFunction(){
         botLine.classList.add("clicked");
         topLine.classList.add("clicked");
     }
+}
+
+async function getPokemon(url){
+    // storing response
+    const response = await fetch(url)
+    // Storing data in the form on JSON
+    let data = await response.json();
+    // Get the results array from the request (results contains pokemon information)
+    newURL = data.next;
+    data = data.results;
+    console.log(data);
+    show(data)
+}
+getPokemon(URL);
+
+function show(data){
+    let content = ``;
+    for (let x = 0; x<data.length; x++){
+        let number = x + 1;
+        let imgLink = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + number + ".png";
+        let name = data[x].name;
+        name = name.toUpperCase();
+        content += `<div class="card">
+        <img src="${imgLink}" alt="...">
+            <div class="textCard">
+                <p>${name}</p>
+                <div class="typeOfPokemon">
+                    <p>Grass</p>
+                    <p>Poison</p>
+                </div>
+            </div>
+        </div>`
+    }
+    document.querySelector(".dataPls").innerHTML = content;
+}
+
+function loadMore(){
+    getPokemon(newURL)
 }
